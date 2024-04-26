727529c0db6edd1fd72d1a760fdb26e72a88213a
Cristian Varela Casas <cristianchantada@gmail.com>
2024-04-26 15:00:02 +0200

    Clase viernes

--- src/main/java/org/varelacasas/mongo/servlets/IndexServlet.java
+++ src/main/java/org/varelacasas/mongo/servlets/IndexServlet.java
@@ -1,9 +1,7 @@
 package org.varelacasas.mongo.servlets;
 
-import org.varelacasas.mongo.controllers.DaoInterface;
 import org.varelacasas.mongo.controllers.PedidoDao;
 import org.varelacasas.mongo.models.pedido;
-
 import jakarta.servlet.annotation.WebServlet;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -21,7 +19,7 @@ public class IndexServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         List<pedido> pedidosLista = pedidoDao.getAll();
         System.out.println("pedidosLista = " + pedidosLista);
-        resp.getWriter().println(pedidosLista);
+        resp.getWriter().println("hola");
 
         req.setAttribute("pedidosLista", pedidosLista);
         getServletContext().getRequestDispatcher("/index.jsp").forward(req, resp);
